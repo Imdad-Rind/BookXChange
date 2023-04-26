78014aaa640fa3b92a6fbdb6439c22916cb44a2a
Huzair-Ahmed <81715737+Huzair-Ahmed@users.noreply.github.com>
2023-04-26 18:17:52 +0500

    setup the all books template

--- src/main/java/com/BookXChange/Model/BookModel.java
+++ src/main/java/com/BookXChange/Model/BookModel.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Setter
 public class BookModel {
     Long BookID;
-    public String BookTitle;
+    String BookTitle;
     String BookAuthor;
     String BookDescription;
     String PublishedYear;
